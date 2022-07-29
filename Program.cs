bb21f446bf142c6b15b05d0518f124d2b760ab9c
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-07-28 21:59:54 -0400

    added C# project from codeacademy

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // This program prints out an ASCII art ghost and bunny!
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| 0 |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("The name of this ghost is Casper!");
+
+      Console.WriteLine(" (\\_/)");
+      Console.WriteLine("(='.'=)");
+      Console.WriteLine("(')_(')");
+      Console.WriteLine("And the name of the bunny above is Jack!");
+     }
+  }
+}
